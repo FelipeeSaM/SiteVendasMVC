343e33a25ee976e362808c9943e50df931e2702d
eu mesmo <luiz_@s>
2022-06-21 00:02:04 -0300

    CRUD scaffolding

---
+++ Data/SiteVendasMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SiteVendasMVC.Models;
+
+namespace SiteVendasMVC.Data
+{
+    public class SiteVendasMVCContext : DbContext
+    {
+        public SiteVendasMVCContext (DbContextOptions<SiteVendasMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteVendasMVC.Models.Department> Department { get; set; }
+    }
+}
